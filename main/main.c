458a32b17f4b843f84fe33e4939fea0e91f68f1f
esteban vargas <110271767+E5t3BAN@users.noreply.github.com>
2022-11-20 21:31:43 -0500

    Add files via upload

---
+++ main/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <driver/gpio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <driver/adc.h>
+#include <esp_log.h>
+
+
+#define motorI1 36
+#define motorD1 39
+#define sensorsharp 16
+#define sensorsharp 17
+#define sensorpiso 34
+#define sonsorpiso 35
+
+
+
+void app_main(void)
+createTask();
+while (1) 
+    {
+    
+    
+    }
+{
+
+}
